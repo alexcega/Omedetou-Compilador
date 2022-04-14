ddc676421b75b601f0feed9c48f1ceea9874bad7
Alex <43385086+alexcega@users.noreply.github.com>
2022-04-13 19:41:44 -0500

    Fase 0 y 1
    Añadimos el documento de propuesta de compilador asi como un ejemplo de un programa usando lark
    fase 0 - Propuesta
    fase 1 - léxico sintáctico

---
+++ omedetou lark.py
@@ -0,0 +1,19 @@
+
+'''
+Alejandro Cedillo Gamez
+A00824742
+13/04/2022
+'''
+from lark import Lark
+
+#objeto lark
+l = Lark(open("tokens omedetou.txt", 'r').read())
+
+#diagrama / arbol del 
+try : 
+
+    my_input = open("ejemplo objeto.txt", 'r').read()
+    print( l.parse(my_input).pretty() )             #funcion pretty para mejor visualizacion
+
+except EOFError:
+    print(EOFError)
