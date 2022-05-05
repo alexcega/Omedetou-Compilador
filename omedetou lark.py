819e9f895a14970543784b0125e67b740484d4d2
Sergio Guasso Idalias <sguasso@live.com>
2022-05-05 17:00:10 -0500

    Acceso a tuplas

--- omedetou lark.py
+++ omedetou lark.py
@@ -16,7 +16,7 @@ from lark import Visitor
 <class 'lark.tree.Tree'>
 '''
 
-
+tupla_aux = ()
 
 my_parser = Lark(open("tokens omedetou.txt", 'r').read())
 
@@ -35,26 +35,40 @@ for somee in my_parse_tree.iter_subtrees_topdown():
     print(somee.data)
 
 class instructions(Visitor):
-    # def escritura(self,tree):
-    #     Quads.append(['print',None,None, pilaO.pop()])
+    def escritura(self,tree):
+        Quads.append(['print',None,None, pilaO.pop()])
     def unnumero(self, tree):
         print(self)
         # print(tree.children)
+
+    # OPERACIONES ARITMETICAS
     def exp_mas_menos(self,tree):
         Poper.append(tree.children[0].value)
         print(tree.children[0])
+    def multiplicacion(self,tree):
+        Poper.append(tree.children[0].value)
+        print(tree.children[0])
+    def division(self,tree):
+        Poper.append(tree.children[0].value)
+        print(tree.children[0])
+
     def var_cte(self,tree):
         print("type",tree.children[0].type)
-        # if tree.children[0].type == 
-        pilaO.append(int(tree.children[0]))
+        if tree.children[0].type == "CONST_INT":
+            tupla_aux = (int(tree.children[0]), 'int')
+            pilaO.append(tupla_aux)
+            print("tuplonga = ",tupla_aux)
+        # pilaO.append(int(tree.children[0]))
         print(tree.children[0]) # el valor de cte
     def expresion(self,args):
         operador = Poper.pop()
         num1 = pilaO.pop()
         num2 = pilaO.pop()
         if(operador == '+'):
-            temp = num1 + num2
-        
+            temp = num1[0] + num2[0] #valor de la tupla
+        elif(operador == '*'):
+            temp = num1[0] * num2[0]
+        pilaO.append(temp)
         Quads.append([operador, num1, num2, temp])
     
         
