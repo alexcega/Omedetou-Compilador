f6593b06db4ebf1d8f4b1560d125cf5d13e5daf7
alexcega <alexcg24600@gmail.com>
2022-05-09 22:28:14 -0500

    Limpieza de codigo general,
    TODOde funciones
    desorden en ejecucion de quads

--- funcionesOmedetou.py
+++ funcionesOmedetou.py
@@ -1,11 +1,237 @@
-null = lambda self, _: None
-tbd = null
+from lark import Visitor
+tbd = 'tbd'
 cont = tbd
 pilaO = []
 Poper = []
 Quads = []
 Psaltos = []
+temp = 1
+myGlobalVars = {}
+myDirFunctions = {}
 
+# Errores#
+def errorValueDontExist(tree):
+    print('Error, no such variable with name "', tree.children[0].value, '" at line ', tree.children[0].line)
+
+
+class instructions(Visitor):
+    #Prints# 
+    def escritura(self,tree):
+        Quads.append(['Print',None,None, pilaO.pop()['value']])
+    
+    def esc2(self, tree):
+        Quads.append(['Print',None,None, pilaO.pop()['value']])
+
+    # declaracion
+    def declaracion_normal(self,tree):
+        # print('pase')
+        myGlobalVars[tree.children[1].children[0].value] = {
+            'type' : tree.children[0].children[0].value,
+            'value' : None,
+            'scope': None
+        }
+        print('declaracion original sin valor')
+        print(myGlobalVars)
+        # print(tree.children[1].children[0])   # name     
+        # print(tree.children[0].children[0])   # type
+
+    # Asignacion
+    def asignacion(self, tree):
+        # try :
+            # print(tree.pretty())
+        myGlobalVars[tree.children[0].value]['value'] = pilaO.pop()['value']
+        Quads.append(['=' , myGlobalVars[tree.children[0].value]['value'], None,tree.children[0].value ])
+
+        # except KeyError:
+        #     errorValueDontExist(tree)
+        #     exit()
+        pass
+
+    def guardar_cte(self, tree):
+        print(tree)
+        # exit()
+        print('se guarda var cte', tree.children[0].value)
+        pilaO.append({'value': tree.children[0].value, 'type':type(tree.children[0].value)})
+        # print(tree)
+
+    def operacion(self, tree):
+        pass
+        # print('operacion ')
+        # print(tree.pretty())
+    '''
+    Inicio de puntos neuralgicos
+    de operaciones aritmeticas
+    '''
+    def np_metermas(self,tree):
+        print('aqui se mete el mas')
+        Poper.append('+')
+
+
+    def np_metermenos(self,tree):
+        print('aqui se mete el menos')
+        Poper.append('-')
+
+    def np_meterpor(self,_tree):
+        print('aqui se mete el por')
+        Poper.append('*')
+
+    def np_meterentre(self,_tree):
+        Poper.append('/')
+        
+    def np_sumarnumeros(self,tree):
+        print(Poper)
+        print(pilaO)
+        if Poper:
+            if Poper[-1] == "+" or  Poper[-1] == "-":
+                right = pilaO.pop()
+                left = pilaO.pop()
+                operador = Poper.pop()
+                # resulttype =  getType(left,right,operador)
+                # print('debe ser', resulttype)
+                
+                # temp = temp + 1
+                if operador == '+':
+                    result =  left['value'] + right['value'] 
+                    pilaO.append({'value':result , 'type':type(result)})
+                    Quads.append([operador, left['value'],right['value'], result])
+                else :
+                    result = right['value'] - left['value']
+                    pilaO.append({'value':result , 'type':type(result)})
+                    Quads.append([operador, left['value'], right['value'], result])
+    
+    def np_multiplicarnumeros(self,tree):
+        print('poper tiene', len(Poper))
+        if Poper:
+            if Poper[-1] == "*" or Poper[-1] == "/":
+                right = pilaO.pop()
+                left = pilaO.pop()
+                operador = Poper.pop()
+                # resulttype =  getType(left,right,operador)
+                # print('debe ser', resulttype)
+                # temp = temp + 1
+                if operador == '*':
+                    result =  left['value'] * right['value']
+                    Quads.append([operador, left['value'], right['value'], result])
+                    pilaO.append({'value':result , 'type':type(result)})
+                else :
+                    result =   left['value'] / right['value']
+                    Quads.append([operador, left['value'], right['value'], result])
+                    pilaO.append({'value':result , 'type':type(result)})
+    
+    '''
+    Puntos neuralgicos 
+    de comparaciones
+    '''
+    
+    def np_meter_mayorque(self,tree):
+        Poper.append('>')
+    
+    def np_meter_menorque(self,tree):
+        Poper.append('<')
+    
+    def np_meter_igual_igual(self,tree):
+        Poper.append('==')
+    
+    def np_meter_no_igual(self, tree):
+        Poper.append('!=')
+
+    def np_meter_and(self, tree):
+        Poper.append('&')
+
+    def np_meter_or(self, tree):
+        Poper.append('|')
+        
+    def np_meter_menor_igual(self, tree):
+        Poper.append('<=')
+    
+    def np_meter_mayor_igual(self, tree):
+        Poper.append('>=')
+        
+    def np_comparacion(self, tree):
+        if Poper:
+            if  Poper[-1] in ['>','<','>=','<=','==','!=']:
+                global temp
+                right = pilaO.pop()
+                left = pilaO.pop()
+                operador = Poper.pop()
+                Quads.append([operador, left['value'], right['value'], 't'+str(temp)])
+                pilaO.append({'value':'t'+str(temp) , 'type':type(True)})
+                temp+=1
+
+    # & , |
+    def np_comparacion_andor(self, tree):
+        if Poper:
+            if  Poper[-1] in ['|','&']:
+                global temp
+                right = pilaO.pop()
+                left = pilaO.pop()
+                print(right)
+                print(left)
+                operador = Poper.pop()
+                # ans =cubo semantico(right,left,operator)
+                # if ans == bool:
+                Quads.append([operador, left['value'], right['value'], 't'+str(temp)])
+                pilaO.append({'value':'t'+str(temp) , 'type':type(True)})
+                temp+=1
+    # NP de while
+    # inicio, condicion y fin
+    def np_iniciowhile(self,tree):
+        Psaltos.append(len(Quads))
+
+    def np_truewhile(self, tree) :
+        condicion = pilaO.pop()
+        if condicion['type'] != bool:
+            print('Syntax Error, expected expresion')
+        else:
+            Quads.append(['Gotof',condicion['value'], None, tbd])
+            Psaltos.append(len(Quads)-1)
+    
+    def np_endwhile(self, tree) :
+        falso = Psaltos.pop()
+        retorno =Psaltos.pop()
+        Quads.append(['Goto', None, None, retorno])
+        Quads[falso][3] = len(Quads)+1
+
+    # NP de if
+    def np_falsoif(self, tree):
+        condicion = pilaO.pop()
+        if condicion['type'] != bool:
+            print('Syntax Error, expected expresion')
+        else:
+            Quads.append(['Gotof',condicion['value'],None, tbd])
+            Psaltos.append(len(Quads)-1)
+
+    def np_finif(self, tree):
+        end = Psaltos.pop()
+        print('se rellena la linea:', end)
+        print('curr quads')
+        for qur in Quads:
+            print(qur)
+        Quads[end][3] = len(Quads)+1
+
+    # NP de else
+    def np_inicioelse(self, tree):
+        Quads.append(['Goto',None,None, tbd])
+        false = Psaltos.pop()
+        Psaltos.append(len(Quads)-1)
+        Quads[false][3] = len(Quads)+1
+    
+
+    # NP de Functions
+    def np_endfunc(self, tree):
+        Quads.append(['EndFunc',None,None,None])
+
+    #TODO implementacion NP de funciones
+    #TODO directorio de funciones
+    # def np_iniciofunc(self, tree):
+    #   crear nombre de funcion en tabla de func
+    #   guardar linea donde inicia
+    #   guardar cantidad de parametros
+    #   ?guardar tipo
+    #   ? tipo de atributos
+    # # 
+
+'''
 # codigo de if
 def condicion():
     falseIf = pilaO.pop()
@@ -53,7 +279,7 @@ def printo():
 #         Poper.apennd('*')
 # Visitor
 
-'''
+
 # call a function with a dictionary
 # https://stackoverflow.com/questions/9168340/using-a-dictionary-to-select-function-to-execute
 # reglas = {
@@ -66,4 +292,6 @@ def printo():
 #     'escritura' : printo,
 #     'esc2' :  printo, 
 #     'factor_var' : '',
-# }'''
+# }
+# 
+# '''
---
+++ omedetouLark.py
@@ -0,0 +1,82 @@
+
+'''
+Alejandro Cedillo A00824742
+Sergio Guasso A00826042
+02/05/2022
+'''
+from cuboSemantico import cuboSemantico, getType
+from cuboSemantico import OTypeError
+from funcionesOmedetou import * 
+from lark import Lark
+from lark import Transformer
+
+'''
+<class 'lark.lexer.Token'>
+<class 'lark.tree.Tree'>
+'''
+
+
+# transformar los tipos de datos del arbol
+class T(Transformer):
+    def entero(self, tok):
+        print(tok)
+        return tok[0].update(value=int(tok[0].value)) 
+
+    def palabra(self,tok):
+        return tok[0].update(value=str(tok[0].value)) 
+    
+    def booleano(self,tok):
+        print(tok[0])
+        return tok[0].update(value=bool(tok[0].value)) 
+    
+    def decimal(self,tok):
+        return tok[0].update(value=float(tok[0].value)) 
+
+my_parser = Lark(open("tokens omedetou.txt", 'r').read())
+
+#diagrama / arbol del 
+try : 
+    my_input = open("Tests/declaracion vars.txt", 'r').read()
+    my_parse_tree = my_parser.parse(my_input)
+except EOFError:
+    print(EOFError)
+
+my_parse_tree = T().transform(my_parse_tree)
+
+# how to print all tokens
+# all_tokens = my_parse_tree.scan_values(lambda v: isinstance(v, lexer.Token))
+# print('alltokens \n', *all_tokens)
+
+
+# aqui comprobamos que se imprime correctamente el tipo de dato de los tokens, 
+# sigue en duda como commprobar cuando tengamos un string ya que todos los demas 
+# tokens en teoria son string
+# # 
+# for mint in my_parse_tree.scan_values(lambda v: isinstance(v, lexer.Token)):
+#     print(mint.value)
+#     print(type(mint.value))
+#     print()
+
+
+# how to print all rules
+# for somee in my_parse_tree.iter_subtrees_topdown():
+#     print(somee.data)
+print(my_parse_tree.pretty())
+
+
+
+
+class operaciones(Visitor):
+    pass
+
+print(my_parse_tree.pretty())
+print()
+
+instructions().visit(my_parse_tree)
+# instructions.visit_topdown(my_parse_tree)
+
+
+print('## Mis cuadruplos')
+for myq in Quads:
+    print(myq)
+print(myGlobalVars)
